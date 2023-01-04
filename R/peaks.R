a34659ddf6974e755a39a1b72f150b6900066580
mniederhuber <m@Wanda.attlocal.net>
2023-01-04 14:57:23 -0500

    finished functions to qfilter and keep only reproducible peaks, started faire.df annotation

--- R/peaks.R
+++ R/peaks.R
@@ -7,10 +7,17 @@ source('utils.R')
 
 load('rData/sheets.rda')
 
+##### load peaks #####
+
 ### get CUT&RUN peaks
 cnr.peaks <- getPeakData(cnr.ss, by = 'id', narrowPeak_colname = 'peak_allFrags')
 cnr.byID <- cnr.peaks %>% GRanges() %>% split(., mcols(.)$id)
+cnr.byGrp <- cnr.peaks %>% GRanges() %>% split(., mcols(.)$Grp)
 
+#filter cnr peaks by q val >= 10 and called in each replicate
+cnr.byGrp <- lapply(cnr.byGrp, function(x) grp_qFilter(x, q = 10))
+
+#make union cnr peak list
 cnr.union <- cnr.byID %>%
   unlist() %>%
   reduce()
@@ -21,25 +28,22 @@ faire.byID <- faire.peaks %>% GRanges() %>% split(., mcols(.)$id) #split by repl
 faire.byGrp <- faire.peaks %>% GRanges() %>% split(., mcols(.)$Grp) #split by pooled replicates
 faire.byExp <- faire.peaks %>% GRanges() %>% split(., mcols(.)$experiment) #split by experiment
 
+#filter by replicate specific qValue and only peaks that intersect between replicates
+faire.byGrp <- lapply(faire.byGrp, function(x) grp_qFilter(x, quantile = 0.75)) 
 
-#filter by replicate specific qValue
-faire.byGrp <- lapply(faire.byGrp, function(x) grp_qFilter(x)) %>% GRangesList()
-
+#make union FAIRE peak list - includes any/all peak calls for all experiments/reps
 faire.union <- faire.byID %>%
   unlist() %>%
   reduce()
 
 
+
+##### annotate peak lists #####
+
 ### build annotated FAIRE peak dataframe
-#TODO - split out osaDegrad reps
-#     - filter FAIRe peaks by qVal - take top X peaks
-#     - write function to filter by qVal and keep on reproducible peaks for each 'grp'
-#What parameters to select "best" most confident peak calls?
-#-- reproducible, high confidence calls, 
-
-
-faire.union %>%
+faire.df <- faire.union %>%
   data.frame() %>%
+  dplyr::filter(!seqnames %in% c('chr4','chrY','chrM')) %>% #drop regions from potentially problematic chromosomes
   dplyr::mutate(peak = 1:nrow(.),
                 assay = 'FAIRE',
                 WT.3LW = ifelse(GRanges(.) %over% faire.byGrp$wt.3LW, T, F),
@@ -47,5 +51,15 @@ faire.union %>%
                 WT.24h = ifelse(GRanges(.) %over% faire.byGrp$wt.24h, T, F),
                 WT.44h = ifelse(GRanges(.) %over% faire.byGrp$wt.44h, T, F),
                 osaGFP.control = ifelse(GRanges(.) %over% faire.byGrp$osaGFP.deGrad_control_faire, T, F),
-                osaGFP.deGrad = ifelse(GRanges(.) %over% faire.byGrp$osaGFP.deGrad_nubG4_faire, T, F),
-                )
+                osaGFP.deGrad = ifelse(GRanges(.) %over% faire.byGrp$osaGFP.deGrad_nubG4_faire, T, F)) %>%
+  dplyr::filter(WT.3LW | WT.6h | WT.24h | WT.24h | WT.44h | osaGFP.control | osaGFP.deGrad) %>% #drop regions that don't have a good quality reproducible peak called
+#get osaGFP deGrad cnr bigwig scores...
+  dplyr::mutate(osaGFP.rep1.spikeNorm = get_bw_score(cnr.ss[cnr.ss$baseName == 'osaGFP-3LW-wing-aGFP-CnR-sup-rep1',]$bigwig_allFrags_sacCer3_spikeNorm[1], GRanges(.)),
+                osaGFP.rep2.spikeNorm = get_bw_score(cnr.ss[cnr.ss$baseName == 'osaGFP-3LW-wing-aGFP-CnR-sup-rep2',]$bigwig_allFrags_sacCer3_spikeNorm[1], GRanges(.)),
+                yw.rep1.spikeNorm = get_bw_score(cnr.ss[cnr.ss$baseName == 'yw-3LW-wing-aGFP-CnR-sup-rep1',]$bigwig_allFrags_sacCer3_spikeNorm[1], GRanges(.)),
+                yw.rep2.spikeNorm = get_bw_score(cnr.ss[cnr.ss$baseName == 'yw-3LW-wing-aGFP-CnR-sup-rep2',]$bigwig_allFrags_sacCer3_spikeNorm[1], GRanges(.)),
+#get faire bigwig scores... 
+                faire.3LW.zScore = get_bw_score(faire.wt.ssPool[faire.wt.ssPool$baseName == 'wt_3LW',]$bigwig_rpgcNorm_zNorm[1], GRanges(.)),
+                faire.6h.zScore = get_bw_score(faire.wt.ssPool[faire.wt.ssPool$baseName == 'wt_6h',]$bigwig_rpgcNorm_zNorm[1], GRanges(.)),
+                faire.24h.zScore = get_bw_score(faire.wt.ssPool[faire.wt.ssPool$baseName == 'wt_24h',]$bigwig_rpgcNorm_zNorm[1], GRanges(.)),
+                faire.44h.zScore = get_bw_score(faire.wt.ssPool[faire.wt.ssPool$baseName == 'wt_44h',]$bigwig_rpgcNorm_zNorm[1], GRanges(.)))
--- R/utils.R
+++ R/utils.R
@@ -1,23 +1,32 @@
-#function to filter qValues - set at 75% cutoff 
+
+#DONE - add functionality to return only qfiltered peaks that are reproduced in each replicate - subsetByOverlaps() ? 
+#wrapper to pass faire peak lists (grangeslist) by experiment with replicate ids to qFilter()
+grp_qFilter <- function(x, quantile = NULL, q = NULL) {
+  grp <- x$grp
+  ids <- unique(x$id) #unique replicate ids
+  
+  #for each unique replicate id, pass experiment peak granges and replicate name to qFilter, then bind the output list
+  grp.filtered <- lapply(ids, function(y) qFilter(x, y, quantile, q)) 
+  
+  grp.list <- grp.filtered %>% GenomicRanges::GRangesList()
+  
+  grp.repShared <- Reduce(IRanges::subsetByOverlaps, grp.list) #takes grangeslist of qval filtered peaks and returns a granges object with only regions shared between replicates
+  
+  return(grp.repShared)  
+}
+
 #takes grouped FAIRE granges object - ie. all replicates for same sample - and filters out bottom 25% peaks by qval per replicate
-qFilter <- function(df, id) {
+qFilter <- function(df, id, quantile = NULL, q = NULL) {
+  
   df %<>% data.frame() %>% dplyr::filter(id == !!id) #filter peak list to specific replicate, "!!" evaluates variable
-  q <- quantile(df$qValue, probs = 0.75) #find the 75% qVal
+  
+  if(!is.null(quantile)) {
+    q <- quantile(df$qValue, probs = quantile) #find the 75% qVal
+  } 
+    
   df.filtered <- df %>% 
     dplyr::filter(qValue >= q) %>% 
     dplyr::mutate(qCutoff = q) 
   
   return(df.filtered) #return the filtered peak list
 } 
-
-#TODO - add functionality to return only qfiltered peaks that are reproduced in each replicate - subsetByOverlaps() ? 
-#wrapper to pass faire peak lists by experiment with replicate ids to qFilter()
-grp_qFilter <- function(x) {
-  grp <- x$grp
-  ids <- unique(x$id) #unique replicate ids
-  
-  #for each unique replicate id, pass experiment peak granges and replicate name to qFilter, then bind the output list
-  grp.filtered <- purrr::map(ids, function(y) qFilter(x, y)) #%>% dplyr::bind_rows()
-   
-  return(grp.filtered)
-}
